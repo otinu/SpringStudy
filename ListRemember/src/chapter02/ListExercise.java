ada04ecb97f8280550e91a3f5fc6f752e09b7f10
otinu <fitshter@gmail.com>
2022-09-11 18:07:01 +0900

    [add]ListExercise

---
+++ ListRemember/src/chapter02/ListExercise.java
@@ -0,0 +1,18 @@
+package chapter02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+
+	public static void main(String[] args) {
+		List<String> prefecture = new ArrayList<>();
+		prefecture.add("北海道");
+		prefecture.add("沖縄県");
+		prefecture.add("香川県");
+		for(String local_name : prefecture) {
+			System.out.println(local_name);
+		}
+	}
+
+}
