1f235f107a75f41f7fdede94e5d562dc1f4586fb
otinu <fitshter@gmail.com>
2022-09-14 08:38:11 +0900

    [add]

---
+++ RequestParamSample/src/main/java/com/example/demo/form/Form.java
@@ -0,0 +1,16 @@
+package com.example.demo.form;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+// lombokによるゲッター/セッターの用意
+@Data
+public class Form {
+	private String name;
+	private Integer age;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate birth;
+}
